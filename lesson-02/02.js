fc004e51bb7ed9c6e257a0af0a513cb4aa4fa065
VeronikaZenchik <veronikazenchik991@gmail.com>
2024-07-29 14:21:04 +0300

    commit

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -15,10 +15,10 @@ let isWinningDouble = false; // your code
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
 
-if ((dice1 === dice2) || ((dice1 > 3) && (dice2 > 3))) {
-  console.log('Выигрышный дубль!')
-  isWinningDouble = true
-} else {
-  console.log('Не выигрышный дубль.')
-  isWinningDouble = true
-}
+// if ((dice1 === dice2) || ((dice1 > 3) && (dice2 > 3))) {
+//   console.log('Выигрышный дубль!')
+//   isWinningDouble = true
+// } else {
+//   console.log('Не выигрышный дубль.')
+//   isWinningDouble = true
+// }
