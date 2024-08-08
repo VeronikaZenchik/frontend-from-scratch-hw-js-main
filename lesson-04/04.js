11dbecc000310711d1fa4bc585ec04c90c1009b5
VeronikaZenchik <veronikazenchik991@gmail.com>
2024-08-08 22:44:31 +0300

    lesson 4 homerowrk 4

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -12,9 +12,12 @@
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
 
-// function doubleEachCharacter(str) {
-
-//   return
-// }
-// console.log(doubleEachCharacter(hello));
-
+function doubleEachCharacter(str) {
+  let newString = '';
+  for(let i = 0; i < str.length; i++) {
+    newString = newString + str[i]
+    newString = newString + str[i] //Второй вызов добавляет еще раз все
+  }
+  return newString
+}
+console.log(doubleEachCharacter('hello'));
