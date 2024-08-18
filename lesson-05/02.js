66699473c5a2371c6dd38c76b3b46a437c385749
VeronikaZenchik <veronikazenchik991@gmail.com>
2024-08-18 16:37:17 +0300

    first homework lesson 5

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -1,38 +1,43 @@
-/*
-Напишите функцию `updateGallery`, которая будет обновлять информацию о произведениях искусства в виртуальной галерее. Функция должна принимать три параметра:
+//
+// Напишите функцию `updateGallery`, которая будет обновлять информацию о произведениях искусства в виртуальной галерее. Функция должна принимать три параметра:
 
-- объект галереи
-- название произведения (ключ)
-- новое значение
+// - объект галереий
+// - название произведения (ключ)
+// - новое значение
 
-Если произведение с таким названием уже есть в галерее, его значение должно быть обновлено. Если произведения нет, оно должно быть добавлено в объект галерею.
+// Если произведение с таким названием уже есть в галерее, его значение должно быть обновлено. Если произведения нет, оно должно быть добавлено в объект галерею.
 
-Пример использования функции:
+// Пример использования функции:
+
+// const gallery = {
+//   'Mona Lisa': 'Leonardo da Vinci',
+//   'Starry Night': 'Vincent van Gogh',
+//   'The Scream': 'Edvard Munch'
+// }
+
+// updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506')
+// updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali')
+
+// console.log(gallery)
+
+// Ожидаемый вывод:
+// {
+//   'Mona Lisa': 'Leonardo da Vinci, 1503-1506',
+//   'Starry Night': 'Vincent van Gogh',
+//   'The Scream': 'Edvard Munch',
+//   'The Persistence of Memory': 'Salvador Dali'
+// }
 
 const gallery = {
   'Mona Lisa': 'Leonardo da Vinci',
   'Starry Night': 'Vincent van Gogh',
-  'The Scream': 'Edvard Munch'
+  'The Scream': 'Edvard Munch',
 }
 
-updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506')
-updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali')
-
+function updateGallery(objectOfGallary, nameOfArt, newValue) {
+  
+}
 console.log(gallery)
 
-Ожидаемый вывод:
-{
-  'Mona Lisa': 'Leonardo da Vinci, 1503-1506',
-  'Starry Night': 'Vincent van Gogh',
-  'The Scream': 'Edvard Munch',
-  'The Persistence of Memory': 'Salvador Dali'
-}
-*/
 
-const gallery = {
-  'Mona Lisa': 'Leonardo da Vinci',
-  'Starry Night': 'Vincent van Gogh',
-  'The Scream': 'Edvard Munch',
-}
 
-function updateGallery() {}
