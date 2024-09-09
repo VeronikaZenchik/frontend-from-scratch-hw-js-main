b34526fef84a019ddfd2969f3ad267702a9b8432
VeronikaZenchik <veronikazenchik991@gmail.com>
2024-09-09 21:59:24 +0300

    lesson 5 hw 1 another way

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -10,13 +10,31 @@
 3. Если ресурс существует, метод должен добавлять значение `amount` к текущему количеству этого ресурса в объекте.
 */
 
+// const game = {
+//   resources: {
+//     gold: 250,
+//     lumber: 100,
+//   },
+//   addResource(resource, amount) {
+//     if (this.resources.hasOwnProperty(resource)){
+//       this.resources[resource] += amount
+//   } else {
+//     console.log("Invalid resource");
+//   }
+//   },
+// }
+// game.addResource("gold", 50)
+// console.log(game.resources);
+
+
+
 const game = {
   resources: {
     gold: 250,
     lumber: 100,
   },
   addResource(resource, amount) {
-    if (this.resources.hasOwnProperty(resource)){
+    if (resource in this.resources){ //  --- можно было так решить
       this.resources[resource] += amount
   } else {
     console.log("Invalid resource");
@@ -24,24 +42,6 @@ const game = {
   },
 }
 game.addResource("gold", 50)
-console.log(game.resources);
-
-
-
-// const game = {
-//   resources: {
-//     gold: 250,
-//     lumber: 100,
-//   },
-//   addResource(resource, amount) {
-//     if ('resources' in resource){ // if (resource in this.resources)  -- можно было так решить
-//       resources[resource] += amount // this.resources[resource] += amount;
-//   } else {
-//     console.log("Invalid resource");
-//   }
-//   },
-// }
-// game.addResource("gold", 50)
 console.log(game.resources);
 
 
